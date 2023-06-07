e616c0c52c737b070bc0814ccfd4239c83179006
Uruanth <perilla174@gmail.com>
2023-06-07 08:07:07 -0500

    TwiNum 1

--- src/main/kotlin/two_sum/TwoSum.kt
+++ src/main/kotlin/two_sum/TwoSum.kt
@@ -8,7 +8,7 @@ fun main() {
 
     val time = measureTime {
         val sol = TwoSum()
-        val outcome = sol.twoSum(intArrayOf(3,3), 6)
+        val outcome = sol.twoSum(intArrayOf(3,3), 6 )
         outcome.forEach { println("Result: $it") }
     }
 
@@ -18,13 +18,18 @@ fun main() {
 
 class TwoSum {
     fun twoSum(nums: IntArray, target: Int): IntArray {
+        val mapa = mutableMapOf<Int, Int>()
         for (num in nums) {
-            val need = target - num
-            if (nums.indexOf(need) >= 0 && nums.indexOf(num) != nums.indexOf(need)) {
-                return intArrayOf(nums.indexOf(num), nums.indexOf(need))
-
-            }
+            if (mapa[num] == null) mapa[num] = 1
+            else mapa.replace(num, mapa[num]!! + 1)
         }
-        return intArrayOf(nums.indexOf(target))
+        mapa.forEach {
+            val need = target - it.key
+            if (need == it.key && it.value > 1) {
+                return intArrayOf(nums.indexOf(it.key), nums.lastIndexOf(need))
+            }
+            if (need != it.key && mapa[need] != null) return intArrayOf(nums.indexOf(it.key), nums.indexOf(need))
+        }
+        return intArrayOf(-1, -1)
     }
 }
